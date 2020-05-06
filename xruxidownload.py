cc17adf9af9d4d8228c4a5334a9e74497983ee87
John Hurley <jhurley@mac.com>
2020-05-05 22:53:00 -0700

    request headers content-length not always there, so guess

--- xruxidownload.py
+++ xruxidownload.py
@@ -2,7 +2,7 @@
 
 __author__  = "github.com/ruxi"
 __license__ = "MIT"
-import requests 
+import requests
 import tqdm     # progress bar
 import os.path
 def download_file(url, filename=False, verbose = False):
@@ -16,24 +16,31 @@ def download_file(url, filename=False, verbose = False):
         local_filename = os.path.join(".",url.split('/')[-1])
     else:
         local_filename = filename
-    r = requests.get(url, stream=True)
-	# print(r.headers)
-    file_size = int(r.headers['Content-Length'])
-    chunk = 1
-    chunk_size=1024
-    num_bars = int(file_size / chunk_size)
-    if verbose:
-        print(dict(file_size=file_size))
-        print(dict(num_bars=num_bars))
 
-    with open(local_filename, 'wb') as fp:
-        for chunk in tqdm.tqdm(
-                                    r.iter_content(chunk_size=chunk_size)
-                                    , total= num_bars
-                                    , unit = 'KB'
-                                    , desc = local_filename
-                                    , leave = True # progressbar stays
-                                ):
-            fp.write(chunk)
+    try:
+        r = requests.get(url, stream=True)
+        # print(r.headers)
+        file_size = int(r.headers.get('Content-Length', 1000000)
+        chunk = 1
+        chunk_size=1024
+        num_bars = int(file_size / chunk_size)
+        if verbose:
+            print(dict(file_size=file_size))
+            print(dict(num_bars=num_bars))
+
+        with open(local_filename, 'wb') as fp:
+            for chunk in tqdm.tqdm(
+                    r.iter_content(chunk_size=chunk_size)
+                    , total= num_bars
+                    , unit = 'KB'
+                    , desc = local_filename
+                    , leave = True # progressbar stays
+            ):
+                fp.write(chunk)
+    except Exception as ee:
+        print(r.headers)
+        print(ee)
+        raise
+
     return
     
