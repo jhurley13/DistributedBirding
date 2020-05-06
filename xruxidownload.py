909c6af0f37396befe3b8f1b10d5fe1e679be362
John Hurley <jhurley@mac.com>
2020-05-05 23:07:01 -0700

    fix missing paren, other formatting issues

--- xruxidownload.py
+++ xruxidownload.py
@@ -1,11 +1,13 @@
 # https://gist.github.com/ruxi/5d6803c116ec1130d484a4ab8c00c603
 
-__author__  = "github.com/ruxi"
+__author__ = "github.com/ruxi"
 __license__ = "MIT"
 import requests
-import tqdm     # progress bar
+import tqdm  # progress bar
 import os.path
-def download_file(url, filename=False, verbose = False):
+
+
+def download_file(url, filename=False, verbose=False):
     """
     Download file with progressbar
     
@@ -13,29 +15,29 @@ def download_file(url, filename=False, verbose = False):
         download_file('http://web4host.net/5MB.zip')  
     """
     if not filename:
-        local_filename = os.path.join(".",url.split('/')[-1])
+        local_filename = os.path.join(".", url.split('/')[-1])
     else:
         local_filename = filename
 
     try:
         r = requests.get(url, stream=True)
         # print(r.headers)
-        file_size = int(r.headers.get('Content-Length', 1000000)
+        file_size = int(r.headers.get('Content-Length', 1000000))
         chunk = 1
-        chunk_size=1024
+        chunk_size = 1024
         num_bars = int(file_size / chunk_size)
         if verbose:
             print(dict(file_size=file_size))
-            print(dict(num_bars=num_bars))
+        print(dict(num_bars=num_bars))
 
+        # leave=True  progressbar stays
         with open(local_filename, 'wb') as fp:
             for chunk in tqdm.tqdm(
                     r.iter_content(chunk_size=chunk_size)
-                    , total= num_bars
-                    , unit = 'KB'
-                    , desc = local_filename
-                    , leave = True # progressbar stays
-            ):
+                    , total=num_bars
+                    , unit='KB'
+                    , desc=local_filename
+                    , leave=True):
                 fp.write(chunk)
     except Exception as ee:
         print(r.headers)
@@ -43,4 +45,3 @@ def download_file(url, filename=False, verbose = False):
         raise
 
     return
-    
